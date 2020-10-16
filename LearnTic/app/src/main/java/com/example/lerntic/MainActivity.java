7e5df18bcd82918c09ec4d4223f4fcadf3803499
htovars <htovars@unal.edu.co>
2020-10-15 23:38:26 -0500

    Apollo implementado para Android Studio

--- LearnTic/app/src/main/java/com/example/lerntic/MainActivity.java
+++ LearnTic/app/src/main/java/com/example/lerntic/MainActivity.java
@@ -4,11 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import org.jetbrains.annotations.NotNull;
+
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getfeedbackByID(1);
+
+    }
+
+    private void getfeedbackByID(int id){
+
+        ApolloConnector.setupApollo().query(
+                FeedbacksQuery
+                        .builder()
+                        .id(id)
+                        .build())
+                .enqueue(new ApolloCall.Callback<FeedbacksQuery.Data>() {
+
+                    @Override
+                    public void onResponse(@NotNull Response<FeedbacksQuery.Data> response) {
+
+                        Log.d(TAG, "Response: " + response.data().feedbackById);
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull ApolloException e) {
+
+                        Log.d(TAG, "Exception " + e.getMessage(), e);
+                    }
+                });
     }
 }
