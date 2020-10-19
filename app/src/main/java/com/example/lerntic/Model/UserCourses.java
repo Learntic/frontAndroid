9392a3cad15c38abdfad045a74f7596b765921f5
htovars <htovars@unal.edu.co>
2020-10-18 20:35:30 -0500

    OwnCourses List

--- app/src/main/java/com/example/lerntic/Model/UserCourses.java
+++ app/src/main/java/com/example/lerntic/Model/UserCourses.java
@@ -1,4 +1,64 @@
 package com.example.lerntic.Model;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.apollographql.apollo.ApolloCall;
+import com.apollographql.apollo.api.Response;
+import com.apollographql.apollo.exception.ApolloException;
+import com.example.lerntic.Model.Objects.user;
+import com.example.lerntic.SignInQuery;
+import com.example.lerntic.type.AccountInput;
+import com.example.lerntic.Model.Objects.course;
+
+import org.jetbrains.annotations.NotNull;
+
 public class UserCourses {
+
+    private final String TAG = "UserCourses";
+    public course[] courses ;
+    public Context context ;
+    public user User;
+
+    public UserCourses(user User, Context context) {
+        this.User = User;
+        this.context = context;
+        GetUserCourser(context);
+    }
+
+    public synchronized void GetUserCourser(final Context context) {
+
+        String username = User.getUsername();
+        String password = User.getToken();
+        User = new user();
+
+        AccountInput account = AccountInput
+                .builder()
+                .username(username)
+                .password(password)
+                .build();
+
+        ApolloConnector.setupApollo().query(
+                SignInQuery
+                        .builder()
+                        .account(account)
+                        .build())
+                .enqueue(new ApolloCall.Callback<SignInQuery.Data>() {
+                    @Override
+                    public void onResponse(@NotNull Response<SignInQuery.Data> response) {
+                        SignInQuery.SignIn data = response.data().signIn();
+                        User.setToken(data.token());
+                        User.setUsername(data.username());
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull ApolloException e) {
+                        Log.d(TAG, "Exception " + e.getMessage(), e);
+                    }
+                });
+    }
+
+    public course[] getcourses() {
+        return courses;
+    }
 }
--- app/src/main/java/com/example/lerntic/View/OwnCourses.java
+++ app/src/main/java/com/example/lerntic/View/OwnCourses.java
@@ -2,15 +2,66 @@ package com.example.lerntic.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.lerntic.Controller.Login_controller;
+import com.example.lerntic.Controller.UserCourses_controller;
+import com.example.lerntic.Model.Objects.course;
+import com.example.lerntic.Model.UserCourses;
 import com.example.lerntic.R;
 
+import java.util.ArrayList;
+
 public class OwnCourses extends AppCompatActivity {
+
+    private UserCourses_controller userCourses_controller = new UserCourses_controller();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_courses);
+        setContentView(R.layout.activity_own_courses);
+        String username = getIntent().getStringExtra("Username");
+        String token = getIntent().getStringExtra("Token");
+
+        TextView textView = (TextView) findViewById(R.id.Own_courses_header);
+        textView.setText(String.format("Cursos de %s", username));
+
+        ListView simpleList = (ListView) findViewById(R.id.coursesList);
+
+        course[] courses;
+
+        /*courses = userCourses_controller.ShowCourses(username,token,getApplicationContext());
+
+        String[] coursesView = new String[courses.length];
+
+        for (int i=0;i<courses.length;i++)
+        {
+            coursesView[i] = courses[i].getname();
+        }*/
+
+        final String animalList[] = {"Lion","Tiger","Monkey","Elephant","Dog","Cat","Camel"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, animalList);
+
+        simpleList.setAdapter(adapter);
+
+        simpleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (animalList[position].equals("Lion")) {
+                    Intent nextActivity = new Intent(OwnCourses.this, MainActivity.class);
+                    startActivity(nextActivity);
+                }else{
+                    Intent nextActivity = new Intent(OwnCourses.this, friends.class);
+                    startActivity(nextActivity);
+                }
+            }
+        });
     }
 }
